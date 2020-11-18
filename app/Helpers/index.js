71e7d2140dcff1af616775be8c4f93afd56c0e78
Italo <contato.italo1020@gmail.com>
2020-11-18 19:16:24 -0300

    adiciona geração de senha aleatoria para recuperacao de senha

--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -5,4 +5,23 @@ const Helpers = use("Helpers")
 /**
  * Generate random string
  * @param { id } - tamanho da string que deseja gerar
+ * @returns { string } retorna um string randomica do tamanho passado
  */
+
+const str_random = async (length = 40) => {
+  let string = ""
+  let len = string.length
+
+  if (len < length) {
+    let size = length - len
+    let bytes = await crypto.randomBytes(size)
+    let buffer = Buffer.from(bytes)
+    string += buffer
+      .toString("base64")
+      .replace(/[^a-zA-Z0-9]/g, "")
+      .substr(0, size)
+  }
+  return string
+}
+
+module.exports = { str_random }
