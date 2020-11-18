33445ba091f45eefccd9bd4ee49db66905d106df
Italo <contato.italo1020@gmail.com>
2020-11-18 14:07:21 -0300

    finaliza controle de categorias

--- app/Controllers/Http/Admin/CategoryController.js
+++ app/Controllers/Http/Admin/CategoryController.js
@@ -1,6 +1,6 @@
 "use strict"
 const Category = use("App/Models/Category")
-
+const Database = use("Database")
 class CategoryController {
   async index({ request, response, pagination }) {
     const title = request.input("title")
@@ -13,13 +13,52 @@ class CategoryController {
     return categories
   }
 
-  async create({ request, response }) {}
+  async store({ request, response }) {
+    const transaction = await Database.beginTransaction()
+    try {
+      const { title, description, image_id } = request.all()
 
-  async show({ request, response }) {}
+      const category = await Category.create(
+        {
+          title,
+          description,
+          image_id,
+        },
+        null,
+        transaction
+      )
+      transaction.commit()
+      if (category) {
+        return category
+      }
+    } catch (error) {
+      transaction.rollback()
+      return response.status(400).send({
+        message: "Erro ao salvar categoria",
+      })
+    }
+  }
 
-  async update({ request, response }) {}
+  async show({ params: { id }, response }) {
+    const category = await Category.findByOrFail("id", id)
+    return response.send(category)
+  }
 
-  async destroy({ request, response }) {}
+  async update({ params: { id }, request, response }) {
+    const { title, description, image_id } = request.all()
+
+    const category = await Category.findByOrFail("id", id)
+
+    category.merge({ title, description, image_id })
+    await category.save()
+    return response.status(200).send(category)
+  }
+
+  async destroy({ params: { id }, response }) {
+    const category = await Category.findByOrFail("id", id)
+    await category.delete()
+    return response.status(204).send()
+  }
 }
 
 module.exports = CategoryController
