9ac29a762c2c05c77849f1e9c4461f5a8f3b2174
Italo <contato.italo1020@gmail.com>
2020-11-18 20:01:37 -0300

    adiciona único e múltiplos uploads de imagem

--- app/Controllers/Http/Admin/ImageController.js
+++ app/Controllers/Http/Admin/ImageController.js
@@ -1,6 +1,71 @@
-'use strict'
-
+"use strict"
+const Image = use("App/Models/Image")
+const { managerSingleUpload, managerMultipleUploads } = use("App/Helpers")
 class ImageController {
+  async index({ request, response, pagination }) {
+    const images = await Image.query
+      .orderBy("id", "DESC")
+      .paginate(pagination.page, pagination.limit)
+
+    return response.send(images)
+  }
+
+  async store({ request, response }) {
+    try {
+      // captura uma ou várias imagens da requisicao
+      const fileJar = request.file("images", {
+        types: ["image"],
+        size: "2mb",
+      })
+
+      // retorno pro usuário
+      let images = []
+
+      // verifica se é um arquivo, se sim, chama o managerSingleUploads
+      // caso contrário, chama o manageMultipleUploads
+      if (!fileJar.files) {
+        const file = await managerSingleUpload(fileJar)
+        if (file.moved()) {
+          const image = await Image.create({
+            path: file.fileName,
+            size: file.size,
+            original_name: file.clientName,
+            extension: file.subtype,
+          })
+          images.push(image)
+          return response.status(201).send({
+            successes: images,
+            errors: {},
+          })
+        }
+        return response.status(400).send({
+          message: "Não foi possível processar esta imagem no momento!",
+        })
+      }
+
+      let files = await managerMultipleUploads(fileJar)
+
+      await Promise.all(
+        files.successes.map(async file => {
+          const image = await Image.create({
+            path: file.fileName,
+            size: file.size,
+            original_name: file.clientName,
+            extension: file.subtype,
+          })
+          images.push(image)
+        })
+      )
+      return response.status(201).send({
+        successes: images,
+        errors: files.errors,
+      })
+    } catch (error) {
+      return response.status(400).send({
+        message: "Não foi possível processar a sua solicitação",
+      })
+    }
+  }
 }
 
 module.exports = ImageController
--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -24,4 +24,56 @@ const str_random = async (length = 40) => {
   return string
 }
 
-module.exports = { str_random }
+/**
+ *  Move um único arquivo para o caminho específico, se não for passado nenhum caminho
+ *  o 'public/uploads' será utilizado
+ * @param { FileJar } file - arquivo a ser gerenciado
+ * @param { string } path - caminho para onde mover o arquivo
+ * @returns { Object<FileJar> }
+ */
+const managerSingleUpload = async (file, path = null) => {
+  path = path ? path : Helpers.publicPath("uploads")
+  //  gera um nome aleatório
+  const random_name = await str_random(30)
+
+  let fileName = `${new Date().getTime}-${random_name}.${file.subtype}`
+
+  // renomeia o arquivo e move para o path
+  await file.move(path, {
+    name: fileName,
+  })
+
+  return file
+}
+
+/**
+ *  Move múltiplos arquivos
+ * @param { FileJar } files - arquivos que serão movidos
+ * @param { string } path - path
+ * @returns { Object  }
+ */
+const managerMultipleUploads = async (fileJar, path = null) => {
+  path = path ? path : Helpers.publicPath("uploads")
+  let successes = []
+  let errors = []
+
+  await Promise.all(
+    fileJar.map(async file => {
+      let random_name = await str_random(30)
+      let fileName = `${new Date().getTime}-${random_name}.${file.subtype}`
+      // move o arquivo
+      await file.move(path, {
+        name: fileName,
+      })
+      // verifica se movel o arquivo
+      if (file.moved()) {
+        successes.push(file)
+      } else {
+        errors.push(file.error())
+      }
+    })
+  )
+  return { successes, errors }
+}
+
+module.exports = { str_random, managerSingleUpload, manageMultipleUploads }
